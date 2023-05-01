84af6fe90876523810f9210fffc2165847fa4f65
veraw <vera.wid@gmail.com>
2023-05-01 15:43:05 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
+import { ProductgridComponent } from './productgrid/productgrid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductgridComponent},
+  {path: 'product/:id', component: ProductdetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
